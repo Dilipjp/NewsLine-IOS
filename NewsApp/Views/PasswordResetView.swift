1ee495e24791e01e2f00da63ed2f89530b04dc45
Dilipjp <jpdilipk@gmail.com>
2024-08-06 00:37:52 +0530

    reset password modified

--- NewsApp/Views/PasswordResetView.swift
+++ NewsApp/Views/PasswordResetView.swift
@@ -14,59 +14,69 @@ struct PasswordResetView: View {
     @Binding var showPasswordReset: Bool
 
     var body: some View {
-        
-        
-        VStack {
-            Text("Reset Password")
-                .font(.largeTitle)
-                .padding()
-
-            TextField("Email", text: $resetEmail)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-
-            if !resetErrorMessage.isEmpty {
-                Text(resetErrorMessage)
-                    .foregroundColor(.red)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.white, Color.yellow]), startPoint: .topLeading, endPoint: .bottomLeading)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("app_icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
                     .padding()
-            }
-
-            Button(action: {
-                sendPasswordReset()
-            }) {
-                Text("Send Reset Link")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    .clipShape(Circle())
+                Text("Reset Password")
+                    .font(.largeTitle)
                     .padding()
-                    .frame(width: 200, height: 50)
-                    .background(Color.green)
-                    .cornerRadius(10)
-            }
-            .padding()
-
-            Spacer()
-
-            Button(action: {
-                showPasswordReset = false
-            }) {
-                Text("Back to Sign In")
-                    .font(.headline)
-                    .foregroundColor(.blue)
+                
+                TextField("Email", text: $resetEmail)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                if !resetErrorMessage.isEmpty {
+                    Text(resetErrorMessage)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+                
+                Button(action: {
+                    sendPasswordReset()
+                }) {
+                    Text("Send Reset Link")
+                        .font(.headline)
+                        .foregroundColor(.yellow)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                }
+                .padding()
+                
+                Spacer()
+                
+                Button(action: {
+                    showPasswordReset = false
+                }) {
+                    Text("Back to Sign In")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                }
+                .padding()
             }
             .padding()
         }
-        .padding()
     }
-
-    func sendPasswordReset() {
-        Auth.auth().sendPasswordReset(withEmail: resetEmail) { error in
-            if let error = error {
-                resetErrorMessage = error.localizedDescription
-            } else {
-                resetErrorMessage = "A password reset link has been sent to your email."
+        
+        func sendPasswordReset() {
+            Auth.auth().sendPasswordReset(withEmail: resetEmail) { error in
+                if let error = error {
+                    resetErrorMessage = error.localizedDescription
+                } else {
+                    resetErrorMessage = "A password reset link has been sent to your email."
+                }
             }
         }
-    }
+    
 }
 
 struct PasswordResetView_Previews: PreviewProvider {
