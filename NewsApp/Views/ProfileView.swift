be4133478dbfb347c06a2eb545e91aec7d740ec7
Dilipjp <jpdilipk@gmail.com>
2024-08-06 01:51:28 +0530

    profile  page modified

--- NewsApp/Views/ProfileView.swift
+++ NewsApp/Views/ProfileView.swift
@@ -1,33 +1,70 @@
 import SwiftUI
+import Firebase
+
 struct ProfileView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
+    @State private var userName: String = ""
 
     var body: some View {
-        VStack {
-            if let userEmail = authViewModel.currentUserEmail() {
-                Text("Welcome, \(userEmail)")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
-            } else {
-                Text("Not signed in")
-                    .font(.title)
-                    .foregroundColor(.red)
-                    .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.white, Color.yellow]), startPoint: .topLeading, endPoint: .bottomLeading)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                if let userEmail = authViewModel.currentUserEmail() {
+                    if !userName.isEmpty {
+                        Text("Welcome \(userName)")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                    } else {
+                        Text("Welcome Email: \(userEmail)")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                } else {
+                    Text("Not signed in")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .padding()
+                }
+                
+                Button(action: {
+                    authViewModel.signOut()
+                }) {
+                    Text("Sign Out")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(Color.black)
+                        .cornerRadius(10)
+                }
+                .padding()
             }
-
-            Button(action: {
-                authViewModel.signOut()
-            }) {
-                Text("Sign Out")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: 200, height: 50)
-                    .background(Color.blue)
-                    .cornerRadius(10)
+            .onAppear {
+                fetchUserName()
             }
-            .padding()
         }
+    }
+
+    func fetchUserName() {
+        guard let userId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        let userRef = Database.database().reference(withPath: "users").child(userId)
+        userRef.observeSingleEvent(of: .value) { snapshot in
+            if let userData = snapshot.value as? [String: Any],
+               let name = userData["username"] as? String {
+                self.userName = name
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+            .environmentObject(AuthViewModel())
     }
 }
